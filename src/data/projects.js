ad6e52135018c84747d7675f7678815833d1ee33
Ahmed50366 <ahmed.mehmood5036@gmail.com>
2025-11-20 18:52:51 +0500

    updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,148 +1,135 @@
 const data = [
   {
     id: '1',
-    title: 'SallyPOS',
+    title: 'Cabo Airport Shuttle',
     description:
-      'Point of Sale (POS) for iOS and Android. Optimized for Smartphones and Tablets. SallyPOS is the ultimate tool to help manage your sales and streamline your operations.',
+      'This app helps Cabo Airport Shuttle drivers work more efficiently. They can log in, scan vouchers, view airport pickups, get updates, add early/late services to a calendar, access vehicle documents and guides, report cleanliness issues, and locate car seats and boosters, all in one place.',
     image: {
-      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1721484550/portfolio/projects/SallyPOS/1111_mfdzqt.png',
+      src: 'https://i.ibb.co/LzYYvwKC/cabo.png',
       placeholderSrc:
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
     },
     links: {
-      site: 'https://apps.apple.com/za/app/sally-pos/id6446204909',
+      site: 'https://play.google.com/store/apps/details?id=com.caboairportshuttle&hl=en',
     },
     technologies: [
-      '#typescript',
-      '#expo',
       '#react-native',
-      '#react-navigation',
-      '#react-native-reanimated',
-      '#payments',
-      '#lottie',
-      '#valtio',
+      '#rtkquery',
       '#i18next',
-      '#firebase',
+      '#onesignal',
     ],
   },
   {
     id: '2',
-    title: 'React Native Yoco',
+    title: 'Beemster',
     description:
-      'Core maintainer and contributor of React Native native module for Yoco SDK. It allows app developers to integrate with Yoco card machines to accept in-person payments from within their application.',
+      'Beemster App is a B2B-exclusive loyalty program designed for retailers who sell Beemster cheese. Retailers can earn points by scanning unique QR codes on purchased cheese products. These points can later be redeemed for rewards, discounts, or exclusive benefits.',
     image: {
-      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1727717856/portfolio/projects/react-native-yoco_b3bjph.png',
+      src: 'https://i.ibb.co/v4HgNW91/beemster.jpg',
       placeholderSrc:
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
     },
     links: {
-      repo: 'https://github.com/tolypash/react-native-yoco',
+      site: 'https://apps.apple.com/us/app/beemster/id1619095017',
     },
     technologies: [
       '#react-native',
-      '#typescript',
-      '#swift',
-      '#kotlin',
-      '#expo modules api',
-      '#turbo modules',
-      '#npm',
-      '#payments',
-      '#sdk',
+      '#redux',
+      "#vision-camera",
+      "#onesignal",
     ],
   },
   {
     id: '3',
-    title: 'Syntho',
+    title: 'Expense Split',
     description:
-      'Syntho is an electronic music education application. Connect with like minded users as you build your own custom profile, communicate and share music via the newsfeed.',
+      'Expense Split is a simple and efficient expense management app that helps users split bills and track shared expenses with friends, family, or roommates. Whether managing group trips, household bills, or outings, the app ensures fair and hassle-free expense tracking.',
     image: {
-      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1671564532/portfolio/projects/Syntho/Screenshot_2022-12-20_at_21.26.17_1_c59hzo.jpg',
+      src: 'https://i.ibb.co/gZPYZy76/split-expense.png',
       placeholderSrc:
         'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
     },
     links: {
-      site: 'https://apps.apple.com/us/app/syntho/id1615419392',
+      site: 'https://apps.apple.com/pk/app/expense-split/id6479750784',
     },
     technologies: [
-      '#typescript',
       '#react-native',
-      '#react-native-web',
-      '#mobx-state-tree',
-      '#expo',
-      '#i18next',
-      '#react-navigation',
+      '#rtkquery',
+      '#firebase',
+      "firebase-messaging",
       '#sentry',
+
     ],
   },
-  {
-    id: '4',
-    title: 'Minfin - Currency Rates',
-    description:
-      'Minfin Currency - application with all necessary information about currency: from currency rates - to the nearest places where you can exchange currency in your city.',
-    image: {
-      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1671563302/portfolio/projects/minfin/Screenshot_2022-12-20_at_21.06.43_vl5h6k.jpg',
-      placeholderSrc:
-        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
-    },
-    links: {
-      site: 'https://apps.apple.com/us/app/minfin-currency-rates/id1574863878',
-    },
-    technologies: [
-      '#typescript',
-      '#react-native',
-      '#mobx-keystone',
-      '#i18next',
-      '#firebase-analytics',
-      '#google-maps',
-    ],
-  },
-  {
-    id: '5',
-    title: 'Apiko Marketplace',
-    description:
-      'An online marketplace made with React. The project was developed on 8th Apiko Intensive.',
-    image: {
-      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
-      placeholderSrc:
-        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
-    },
-    links: {
-      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app',
-    },
-    technologies: [
-      '#react',
-      '#redux',
-      '#socket.io',
-      '#scss',
-      '#formik',
-      '#react-window',
-      '#restapi',
-    ],
-  },
-  {
-    id: '6',
-    title: 'eCommerce Mobile App',
-    description:
-      'A Mobile eCommerce application built with React Native that includes cart functionality, push notifications, user authentication and user orders',
-    image: {
-      src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348073/portfolio/projects/rn-shop/rn-shop-lg_fofglf.jpg',
-      placeholderSrc:
-        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
-    },
-    links: {
-      repo: 'https://github.com/vsnaichuk/RN-shop',
-    },
-    technologies: [
-      '#react-native',
-      'expo',
-      '#redux',
-      '#redux-toolkit',
-      '#react-navigation',
-      '#nodejs',
-      '#express',
-      '#mongodb',
-    ],
-  },
+  // {
+  //   id: '4',
+  //   title: 'Minfin - Currency Rates',
+  //   description:
+  //     'Minfin Currency - application with all necessary information about currency: from currency rates - to the nearest places where you can exchange currency in your city.',
+  //   image: {
+  //     src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1671563302/portfolio/projects/minfin/Screenshot_2022-12-20_at_21.06.43_vl5h6k.jpg',
+  //     placeholderSrc:
+  //       'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+  //   },
+  //   links: {
+  //     site: 'https://apps.apple.com/us/app/minfin-currency-rates/id1574863878',
+  //   },
+  //   technologies: [
+  //     '#typescript',
+  //     '#react-native',
+  //     '#mobx-keystone',
+  //     '#i18next',
+  //     '#firebase-analytics',
+  //     '#google-maps',
+  //   ],
+  // },
+  // {
+  //   id: '5',
+  //   title: 'Apiko Marketplace',
+  //   description:
+  //     'An online marketplace made with React. The project was developed on 8th Apiko Intensive.',
+  //   image: {
+  //     src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+  //     placeholderSrc:
+  //       'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+  //   },
+  //   links: {
+  //     repo: 'https://github.com/vsnaichuk/apiko-marketplace-app',
+  //   },
+  //   technologies: [
+  //     '#react',
+  //     '#redux',
+  //     '#socket.io',
+  //     '#scss',
+  //     '#formik',
+  //     '#react-window',
+  //     '#restapi',
+  //   ],
+  // },
+  // {
+  //   id: '6',
+  //   title: 'eCommerce Mobile App',
+  //   description:
+  //     'A Mobile eCommerce application built with React Native that includes cart functionality, push notifications, user authentication and user orders',
+  //   image: {
+  //     src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348073/portfolio/projects/rn-shop/rn-shop-lg_fofglf.jpg',
+  //     placeholderSrc:
+  //       'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+  //   },
+  //   links: {
+  //     repo: 'https://github.com/vsnaichuk/RN-shop',
+  //   },
+  //   technologies: [
+  //     '#react-native',
+  //     'expo',
+  //     '#redux',
+  //     '#redux-toolkit',
+  //     '#react-navigation',
+  //     '#nodejs',
+  //     '#express',
+  //     '#mongodb',
+  //   ],
+  // },
 ];
 
 export default data;

c35379eb6c9620a88551fb590e07a1ca77cfd730
Ahmed Mehmood <96591045+Ahmed50366@users.noreply.github.com>
2025-12-15 01:07:23 +0500

    Update resume link to Google Drive

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -11,7 +11,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const resumeLink =
-  '/src/assets/Ahmed-Resume.pdf';
+  'https://drive.google.com/file/d/1gjlDPh0exWQiWojdtAfY7HsLIAZIc6bA/preview';
 
 const Resume = () => {
   const pdfWrapper = useRef(null);
